2eb1e61b4e7d8c3e6a515add1bcadde8c5f021cd
arunsagarrai01 <arunsagarrai6@gmail.com>
2026-09-04 19:57:06 +0545

    Learning to built ecommerce platform using provider also learning the concept of provider in dart

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:ecommerce/screens/home_screens.dart';
+import 'package:flutter/material.dart';
+
+void main(runApp){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreens(),
+    );
+  }
+}
